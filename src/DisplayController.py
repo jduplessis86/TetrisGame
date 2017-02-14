0feee8910b7fa2239540c3c6f66573b8d6f80401
Joseph DuPlessis <jduplessis86@gmail.com>
2017-02-14 00:39:32 -0500

    Added a little display and input func

---
+++ src/DisplayController.py
@@ -0,0 +1,35 @@
+from abc import ABC, abstractmethod
+
+
+class DisplayControllerInterface():
+
+	def _init_(self):
+		self.maxHight = 20
+		self.maxWidth = 20
+
+	def clearScreen(self):
+		if os.name == "nt":
+			os.system('cls')  # For Windows
+		else:
+			os.system('clear')  # For Linux/OS X
+
+	def drawSquar(self,side,x,y):
+		"""Upper Left Cords """
+		for i in range(y):
+			print("\n")
+		for i in range(side):
+			line = ""
+			for j in range(x):
+				line += " "
+			for k in range (side):
+				line += "#"
+			print(line)
+
+
+
+
+
+
+
+def printLine():
+	print("diplay")
