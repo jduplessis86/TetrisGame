bc5c0ab65cf8f793f87e7a3f69f852e02167467b
Joseph DuPlessis <jduplessis86@gmail.com>
2017-02-14 00:51:25 -0500

    Update main

--- src/Main.py
+++ src/Main.py
@@ -1,5 +1,45 @@
 
+import time
+import threading
+import os
+import Getch
+import sys
+from DisplayController import DisplayControllerInterface 
 
+lock = threading.Lock()
+
+def input_thread(list):
+	while True:
+		time.sleep(0.5)
+		lock.acquire()
+		try:
+			char = Getch.getch()
+		finally:
+			lock.release()
+		if char == 'q':
+			list.append(None)
+			return
+        
+
+def do_stuff(c):
+    list = []
+    index = 0
+    t = threading.Thread(target=input_thread, args =(list,))
+    t.start()
+    while not list:
+        index += 1
+        index = index % 15
+        c.drawSquar(3,10,index)
+        time.sleep(1)
+
+
+	
 
 if __name__ == "__main__":
-	print("Welcome to Tetris")
+
+	controler = DisplayControllerInterface()
+	
+	do_stuff(controler)
+
+
+
